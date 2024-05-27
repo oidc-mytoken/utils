ff78b3b8b7c05f31f6e6950c3783d79105b1455d
zachmann <gabriel.zachmann@kit.edu>
2024-05-27 14:48:33 +0200

    change GetFromJWT

--- utils/jwtutils/jwtutils.go
+++ utils/jwtutils/jwtutils.go
@@ -8,29 +8,25 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// ResultSet is a slice of values with an additional value indicating if it was set
-type ResultSet []struct {
-	Value interface{}
-	Set   bool
-}
-
-// GetFromJWT returns the values for the requested keys from the JWT
-func GetFromJWT(rlog log.Ext1FieldLogger, token string, key ...string) (values ResultSet) {
-	if atJWT, _ := jwt.Parse(token, nil); atJWT != nil {
-		rlog.Trace("Parsed token")
-		if claims, ok := atJWT.Claims.(jwt.MapClaims); ok {
-			for _, k := range key {
-				v, set := claims[k]
-				values = append(
-					values, struct {
-						Value interface{}
-						Set   bool
-					}{
-						Value: v,
-						Set:   set,
-					},
-				)
-			}
+// GetFromJWT returns the values for the requested keys from the JWT (or all)
+func GetFromJWT(rlog log.Ext1FieldLogger, token string, key ...string) map[string]interface{} {
+	atJWT, _ := jwt.Parse(token, nil)
+	if atJWT == nil {
+		return nil
+	}
+	rlog.Trace("Parsed token")
+	claims, ok := atJWT.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil
+	}
+	if len(key) == 0 {
+		return claims
+	}
+	values := make(map[string]interface{}, len(key))
+	for _, k := range key {
+		v, set := claims[k]
+		if set {
+			values[k] = v
 		}
 	}
 	return values
@@ -38,40 +34,29 @@ func GetFromJWT(rlog log.Ext1FieldLogger, token string, key ...string) (values R
 
 // GetValueFromJWT returns the value for the given key
 func GetValueFromJWT(rlog log.Ext1FieldLogger, token, key string) interface{} {
-	res := GetFromJWT(rlog, token, key)
-	if len(res) != 1 {
-		return nil
-	}
-	if !res[0].Set {
-		return nil
-	}
-	return res[0].Value
+	res := GetFromJWT(rlog, token)
+	return res[key]
 }
 
 // GetStringFromJWT returns a string value for the given key
 func GetStringFromJWT(rlog log.Ext1FieldLogger, token, key string) (string, bool) {
-	res := GetFromJWT(rlog, token, key)
-	if len(res) != 1 {
+	res := GetFromJWT(rlog, token)
+	v, ok := res[key]
+	if !ok {
 		return "", false
 	}
-	if !res[0].Set {
-		return "", false
-	}
-	v, ok := res[0].Value.(string)
-	return v, ok
+	vv, ok := v.(string)
+	return vv, ok
 }
 
 // GetAudiencesFromJWT parses the passed jwt token and returns the aud claim as a slice of strings
 func GetAudiencesFromJWT(rlog log.Ext1FieldLogger, token string) ([]string, bool) {
 	rlog.Trace("Getting auds from token")
-	res := GetFromJWT(rlog, token, "aud")
-	if len(res) != 1 {
+	res := GetFromJWT(rlog, token)
+	auds, found := res["aud"]
+	if !found {
 		return nil, false
 	}
-	if !res[0].Set {
-		return nil, false
-	}
-	auds := res[0].Value
 	switch v := auds.(type) {
 	case string:
 		return []string{v}, true
