39f4856ac8054a7583a1f1b8f52ffbfd613c0e68
zachmann <gabriel.zachmann@kit.edu>
2025-02-11 11:52:39 +0100

    [httpclient] do not enable debug on Init

--- httpclient/httpClient.go
+++ httpclient/httpClient.go
@@ -4,7 +4,6 @@ import (
 	"time"
 
 	"github.com/go-resty/resty/v2"
-	log "github.com/sirupsen/logrus"
 	"github.com/valyala/fasthttp"
 
 	"github.com/oidc-mytoken/utils/context"
@@ -27,13 +26,15 @@ func Init(hostURL, userAgent string) {
 	if hostURL != "" {
 		client.SetBaseURL(hostURL)
 	}
-	if log.IsLevelEnabled(log.DebugLevel) {
-		client.SetDebug(true)
-	}
 	client.SetHeader(fasthttp.HeaderUserAgent, userAgent)
 }
 
 // Do returns the client, so it can be used to do requests
 func Do() *resty.Client {
 	return client
+}
+
+// EnableDebug enables debug on the client
+func EnableDebug() {
+	client.SetDebug(true)
 }
